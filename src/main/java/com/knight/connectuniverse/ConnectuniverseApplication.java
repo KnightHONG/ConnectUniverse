c617e427759f8cb89643d78ed341ac9cc645ce9d
黄健洪 <3324045781@qq.com>
2021-07-31 16:01:06 +0800

    初始化项目

---
+++ src/main/java/com/knight/connectuniverse/ConnectuniverseApplication.java
@@ -0,0 +1,13 @@
+package com.knight.connectuniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectuniverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectuniverseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knight/connectuniverse/ConnectuniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knight.connectuniverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectuniverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
